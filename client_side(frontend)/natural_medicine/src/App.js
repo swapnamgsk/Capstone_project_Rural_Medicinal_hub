8c15f6cc8bda000cb6f4451c961db9f5359b0966
swapnamgsk <mgswapna1911@gmail.com>
2023-06-07 01:30:44 +0530

    register page compleated

--- client_side(frontend)/natural_medicine/src/App.js
+++ client_side(frontend)/natural_medicine/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Home';
+import Signup from './Components/SignupPage';
+import Login from './Components/Loginpage';
+
 function App() {
   return (
     <div className="App">
-      <Home/>
+       {/* <Home/>  */}
+        <Signup/>
+        {/* <Login/> */}
     </div>
   );
 }
--- client_side(frontend)/natural_medicine/src/Components/Home.js
+++ client_side(frontend)/natural_medicine/src/Components/Home.js
@@ -13,7 +13,7 @@ function Home()
                 <h2 id = "signup">Signup</h2>
             </div>
             <div id = "box1">
-                <h1>A Journey into Rural Medicinal Plant Knowledge</h1>
+                <h1 id = "text">  A Journey into Rural Medicinal<br/>Plant Knowledge</h1>
             </div>
             <img src = "circles.png" id = "two"></img>
             <div id = "box2"></div>
@@ -23,6 +23,17 @@ function Home()
                     <p>Historically plants have played an important role in medicine. Through observation and experimentation, human beings have learnt that plants promote health and well-being. The use of these herbal remedies is not only cost effective but also safe and almost free from serious side effects. The village elders, farmers and tribal have tremendous knowledge about for health reasons started thousands of years ago and is still part of medical practices by folks of various regions of Indian sub-continents as well as several other countries including China middle East, Africa Egypt, South America and other developing countries of world. This review article shade a small beam of light on conservational strategies of medicinal plants as well as their marketing price in past and near future scenario.</p>
                 </div>
             </div>
+            <div id = "threebox">
+                <div class = "same" id = "first"></div>
+                <div class = "same"></div>
+                <div class = "same"></div>
+            </div>
+            <p  id = "big">The information that has been received from the National Biodiversity Authority, Government of India has enacted the Biological Diversity Act, 2002 for the regulation of the access to biological resources and/or associated knowledge for specific activities that the user of the biological resources needs to share the advantages in an equitable and fair manner with the provider of the biological resources.
+
+Ayush industry owners/extract manufacturers/exporters, etc. for participation in the cultivation of medicinal plants with a pre-decided price, buy-back agreement, and volume. They will share cultivation costs on the terms that are agreeable to the parties, clusters, farmers, industry owners, etc. 
+
+</p>
+            <div id = "footer"></div>
         </div>
     )
 }
