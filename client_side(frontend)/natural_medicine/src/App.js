f33098a8c9244c87417de9674dd817a48a979be3
swapnamgsk <mgswapna1911@gmail.com>
2023-07-04 21:31:32 +0530

    fix: init

--- client_side(frontend)/natural_medicine/src/App.js
+++ client_side(frontend)/natural_medicine/src/App.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import './App.css';
-// import BackEnd from './Components/BackEnd'
 
 
+import Categories from './Components/CategoriesFrontend';
 // import Home from './Components/Home';<br></br>
 // import Signup from './Components/Signup';<br></br>
 // import Login from './Components/Login';<br></br>
 // import Forgot from './Components/Forgot';<br></br>
 // import About from './Components/About';<br></br>
-import PlantResource from './Components/PlantResouce';
+// import PlantResource from './Components/PlantResouce';
 
 
 function App() {
   return (
     <div className="App">
-        {/* <BackEnd /> */}
-
-
-
+      
+      <Categories />
       {/* <Home/><br></br> */}
       {/* <Signup/><br></br> */}
       {/* <Login/><br></br> */}
       {/* <Forgot/><br></br> */}
       {/* <About/><br></br> */}
-      <PlantResource/>
-
-
+      {/* <PlantResource/> */}
 
     </div>
   );
